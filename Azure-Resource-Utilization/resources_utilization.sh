a80eae6ce19d11340c7c261c689cf8d75e657489
OktaySavdi <oktay.savdi@gmail.com>
2024-01-28 16:50:19 +0100

    Update resources_utilization.sh

--- Azure-Resource-Utilization/resources_utilization.sh
+++ Azure-Resource-Utilization/resources_utilization.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+#set -x
+#set -e
+
 # Main function to call other functions based on the resource type
 subscription_id=$1
 
 # Array to track processed storage accounts for each subscription
 declare -A processed_accounts
+declare -A processed_subscriptions
 
 az login --service-principal --username {{ username }} --password {{ password }} --tenant {{ tenant }}
 
@@ -38,9 +42,36 @@ find_orphaned_resources() {
     fi
 }
 
+# Function to find orphaned resources for a given resource type
+find_orphaned_snapshot() {
+    local resource_type=$1
+    local query_expression=$2
+    local subscription_id=$3
+    local output_added=false
+
+    # Get the subscription by ID
+    local subscription_name=$(az account show --subscription $subscription_id --query "{Name:name}" --output tsv)
+
+    local orphaned_resources=($(az $resource_type list --subscription $subscription_id --query "$query_expression" --output tsv))
+
+    if [ ${#orphaned_resources[@]} -gt 0 ]; then
+        if [ "$output_added" = false ]; then
+                echo "************************** Snapshots older than 7 days ************************" >> $out_file
+                output_added=true
+        fi
+        echo "[$subscription_name] - Orphaned $resource_type" >> $out_file
+
+        for orphaned_resource in "${orphaned_resources[@]}"; do
+            echo "$orphaned_resource" >> $out_file
+        done
+        echo "*****************************************************************" >> $out_file
+    fi
+}
+
 # Function to find orphaned Subnets
 find_orphaned_subnets() {
     local subscription_id=$1
+    local output_added=false
 
     # Get the subscription by ID
     local subscription_name=$(az account show --subscription $subscription_id --query "{Name:name}" --output tsv)
@@ -56,15 +87,18 @@ find_orphaned_subnets() {
         local orphaned_subnets=($(az network vnet subnet list --subscription $subscription_id --vnet-name $vnet_name --resource-group $resource_group --query '[?ipConfigurations[0]==`null`].id' --output tsv))
 
         if [ ${#orphaned_subnets[@]} -gt 0 ]; then
-            echo "[$subscription_name] - Orphaned Subnets:" >> $out_file
+           if [ "$output_added" = false ]; then
+                echo "[$subscription_name] - Orphaned Subnets:" >> $out_file
+                output_added=true
+            fi            
             for subnet in "${orphaned_subnets[@]}"; do
                 if [ -n "$subnet" ]; then
                     echo "$subnet" >> $out_file
                 fi
             done
         fi
-        echo "*****************************************************************" >> $out_file
     done
+    echo "*****************************************************************" >> $out_file
 }
 
 # Function to find orphaned Resource Groups
@@ -125,7 +159,7 @@ check_vm_utilization() {
         # Check if the difference is over 40%
         if (( $(echo "$cpuUtilization < 30" | bc -l) )) || (( $(echo "$memoryUtilization < 40" | bc -l) )); then
            if [ "$output_added" = false ]; then
-                echo "************************** Performance (Last 3 Months) ************************" >> $out_file
+                echo "************************** VMs Performance (Last 3 Months) ************************" >> $out_file
                 output_added=true
             fi
             echo -e "[$subscription_name] - [Percentage CPU] $cpuUtilization% and [Percentage Memory] $memoryUtilization%" >> $out_file
@@ -137,49 +171,45 @@ check_vm_utilization() {
 
 check_storage_accounts() {
     local subscription_id=$1
-
     az account set --subscription=$subscription_id
 
     # Get a list of storage accounts in the subscription
     storage_accounts=$(az storage account list --query '[].name' --output tsv)
-    
-    # Loop through each storage account
+
     for storage_account in $storage_accounts; do
-    
-        account_key=$(timeout 10 az storage account keys list --account-name $storage_account --query '[0].value' --output tsv)
-        
-        # Get a list of containers in the storage account
-        containers=$(timeout 10 az storage container list --account-name $storage_account --account-key $account_key --query '[].name' --output tsv)
-    
-        # Get the list of file shares in the storage account
-        fileShares=$(timeout 10 az storage share list --account-name $storage_account --account-key $account_key --query '[].name' --output tsv)
-    
-        if [ ! -z "$containers" ]; then
-            # Loop through each container
-            for container in $containers; do
-                # Get total size of all blobs in the specified container
-                total_size=$(timeout 10 az storage blob list --account-name $storage_account --container-name $container --account-key $account_key --query "[].properties.contentLength" --output tsv | paste -sd+ - | bc)
-                # Convert bytes to gigabytes for better readability
-                total_size_gb=$(echo "scale=2; $total_size / (1024*1024*1024)" | bc)
-                
-                check_and_print $storage_account $container $total_size_gb "Blob" $subscription_id
-            done
-        fi
-    
-        if [ ! -z "$fileShares" ]; then
-            # Loop through each file share
-            for share in $fileShares; do
-                usedCapacity=$(timeout 10 az storage share stats --account-name $storage_account --account-key $account_key  --name $share --query 'usageStats[0].usageInBytes' --output tsv)
-                # Convert bytes to gigabytes for better readability
-                used_capacity_gb=$(echo "scale=2; $usedCapacity / (1024*1024*1024)" | bc)
-                
-                check_and_print $storage_account $share $used_capacity_gb "FileShare" $subscription_id
-            done
-        fi
+        account_key=$(timeout 60 az storage account keys list --account-name $storage_account --query '[0].value' --output tsv)
+        check_storage_account $storage_account $account_key $subscription_id
+    done
+    echo "*****************************************************************" >> $out_file
+}
+
+check_storage_account() {
+    local storage_account=$1
+    local account_key=$2
+    local subscription_id=$3
+
+    # Get the subscription by ID
+    local subscription_name=$(az account show --subscription $subscription_id --query "{Name:name}" --output tsv)
+
+    # Get a list of containers in the storage account
+    containers=$(timeout 60 az storage container list --account-name $storage_account --account-key $account_key --query '[].name' --output tsv)
+
+    for container in $containers; do
+        total_size=$(timeout 60 az storage blob list --account-name $storage_account --container-name $container --account-key $account_key --query "[].properties.contentLength" --output tsv | paste -sd+ - | bc)
+        total_size_gb=$(echo "scale=2; $total_size / (1024*1024*1024)" | bc)
+        check_and_print $storage_account $container $total_size_gb "Blob" $subscription_id
+    done
+
+    # Get the list of file shares in the storage account
+    fileShares=$(timeout 60 az storage share list --account-name $storage_account --account-key $account_key --query '[].name' --output tsv)
+
+    for share in $fileShares; do
+        usedCapacity=$(timeout 60 az storage share stats --account-name $storage_account --account-key $account_key --name $share --query 'usageStats[0].usageInBytes' --output tsv)
+        used_capacity_gb=$(echo "scale=2; $usedCapacity / (1024*1024*1024)" | bc)
+        check_and_print $storage_account $share $used_capacity_gb "FileShare" $subscription_id
     done
 }
 
-# Function to check if total size is over 100 GB and print the result
 check_and_print() {
     local storage_account=$1
     local container_or_share=$2
@@ -187,18 +217,23 @@ check_and_print() {
     local type=$4
     local subscription_id=$5
 
-    # Get the subscription by ID
+    # Get the subscription name by ID
     local subscription_name=$(az account show --subscription $subscription_id --query "{Name:name}" --output tsv)
 
     if (( $(echo "$total_size_gb > 100 || $total_size_gb == 0" | bc -l) )); then
-       # Check if the storage account has been processed for this subscription
-       if [[ "${processed_accounts["$storage_account"]}" != "true" ]]; then
-           echo -e "[$subscription_name] - Storage Account Consumption:" >> $out_file
-           echo -e $storage_account >> $out_file
-           # Mark the storage account as processed for this subscription
-           processed_accounts["$storage_account"]="true"
-       fi
-       echo -e "\t- $type: $container_or_share  Used Capacity: ${total_size_gb} GB" >> $out_file
+        # Check if the subscription header has been printed
+        if [[ "${processed_subscriptions["$subscription_name"]}" != "true" ]]; then
+            echo -e "[$subscription_name] - Storage Account Consumption:" >> "$out_file"
+            processed_subscriptions["$subscription_name"]="true"
+        fi
+        # Check if the storage account has been processed for this subscription
+        if [[ "${processed_accounts["$storage_account"]}" != "true" ]]; then
+            echo -e $storage_account >> $out_file
+            # Mark the storage account as processed for this subscription
+            processed_accounts["$storage_account"]="true"
+        fi
+
+        echo -e "\t- $type: $container_or_share  Used Capacity: ${total_size_gb} GB" >> "$out_file"
     fi
 }
 
@@ -218,7 +253,7 @@ if [ -z "$subscription_id" ]; then
         find_orphaned_subnets $subscription_id
         find_orphaned_resources "network nat gateway" '[?subnets==`null`].id' $subscription_id
         find_orphaned_resources "network application-gateway" "[?backendAddressPools==null && frontendIpConfigurations==null].id" $subscription_id
-        find_orphaned_resources "snapshot" "[?timeCreated<='$(date -u -d '7 days ago' +'%Y-%m-%dT%H:%MZ')'].id" $subscription_id
+        find_orphaned_snapshot "snapshot" "[?timeCreated<='$(date -u -d '7 days ago' +'%Y-%m-%dT%H:%MZ')'].id" $subscription_id
         find_orphaned_resource_groups $subscription_id
         check_vm_utilization $subscription_id
         check_storage_accounts $subscription_id
@@ -237,7 +272,7 @@ else
         find_orphaned_subnets $subscription_id
         find_orphaned_resources "network nat gateway" '[?subnets==`null`].id' $subscription_id
         find_orphaned_resources "network application-gateway" "[?backendAddressPools==null && frontendIpConfigurations==null].id" $subscription_id
-        find_orphaned_resources "snapshot" "[?timeCreated<='$(date -u -d '7 days ago' +'%Y-%m-%dT%H:%MZ')'].id" $subscription_id
+        find_orphaned_snapshot "snapshot" "[?timeCreated<='$(date -u -d '7 days ago' +'%Y-%m-%dT%H:%MZ')'].id" $subscription_id
         find_orphaned_resource_groups $subscription_id
         check_vm_utilization $subscription_id
         check_storage_accounts $subscription_id
